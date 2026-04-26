500a831f92a23686c7caccdc58a5e623d51dea0a
memiik <memiik@protonmail.com>
2026-04-26 19:06:59 +0300

    create new spring module - user-service

---
+++ user-service/src/test/java/com/instance/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instance.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
